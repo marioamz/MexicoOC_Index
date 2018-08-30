1568198020bc811c2b74d60673f432b89aa1f9d3
Mario Moreno <marioamz@gmail.com>
2018-08-30 17:05:36 -0500

    pca done!

--- exploration_pca_ca.py
+++ exploration_pca_ca.py
@@ -1,9 +1,3 @@
-"""
-Created on Mon Apr  9 17:47:05 2018
-
-@author: mariomoreno
-"""
-
 import pandas as pd
 import re
 import seaborn as sns
@@ -241,33 +235,118 @@ def graph_crosstab(df, col1, col2):
 
 ### PRINCIPAL COMPONENT ANALYSIS ###
 
-def standardize(df):
+def pca(df, components):
     '''
-    This function takes in a dataframe and standardizes all
-    the variables, which is a necessary step for deploying
-    and optimizing machine learning algorithms like PCA
-
-    It returns a list of arrays that are the standardized
-    columns
+    This function takes in a dataframe and the number of principal
+    components we want to find in that dataframe, and returns a PCA
+    class which is our decomposed dataframe with the number of
+    components and an array of all the principal components as
+    vectors
     '''
 
     std_array = preprocessing.StandardScaler().fit_transform(df)
-    return std_array
-
-
-def pca(array, components):
-    '''
-    This function takes in the arrays that have been standardized
-    and the number of components we want. From there, it calculates
-    the principal component vectors and how much of the variation
-    they explain.
-    '''
-
     pc = decomposition.PCA(n_components = components)
-    y_pca = pc.fit_transform(array)
+    y_pca = pc.fit_transform(std_array)
 
+    return pc, y_pca
+
+
+def pca_df(df, pc, y_pca, components, var1, var2):
+    '''
+    This function takes in a dataframe without strings, the principal
+    components class we created above, the array of vectors, the
+    number of components already established, and our target variable(s).
+
+    It returns a  pandas dataframe where the columns are the principal
+    component vectors
+    '''
+
+    # graphing and printing the explained variance
+    explained_variance_graph(pc)
     print('Variance explained for first' + ' ' + str(components) + \
     ' ' + 'components:')
     print(pc.explained_variance_ratio_)
 
-    return y_pca
+    # creating the dataframe of PC vectors
+    pcaDF = pd.DataFrame(data = y_pca, columns = \
+    ['PC' + str(i+1) for i in range(components)])
+
+    # creating final dataframe
+    final = pd.concat([pcaDF, df[var1], df[var2]],  axis=1)
+
+    return final
+
+
+def explained_variance_graph(vectors):
+    '''
+    This function takes decomposed vectors and returns
+    a cumulative explained variance graph for the number
+    of components specified above
+    '''
+
+    plt.plot(np.cumsum(vectors.explained_variance_ratio_))
+    plt.xlabel('number of components')
+    plt.ylabel('cumulative explained variance')
+    plt.show()
+
+
+def two_dimension_pca_graph(df, var1):
+    '''
+    This function takes in a dataframe with principal vectors as
+    columns, as well as the variable of interest that we'd like to
+    graph which should also ideally be in the df passed in.
+
+    It plots it on a grid graph
+    '''
+
+    fig = plt.figure(figsize = (16,16))
+    ax = fig.add_subplot(1,1,1)
+    ax.set_xlabel('Principal Component 1', fontsize = 15)
+    ax.set_ylabel('Principal Component 2', fontsize = 15)
+    ax.set_title('2 Component PCA', fontsize = 20)
+
+
+    targets = df[var1].unique().tolist()
+    colors = sns.color_palette('husl', n_colors=32)  # a list of RGB tuples
+    for target, color in zip(targets,colors):
+        indicesToKeep = df[var1] == target
+        ax.scatter(df.loc[indicesToKeep, 'PC1']
+               , df.loc[indicesToKeep, 'PC2']
+               , c = color
+               , s = 100)
+    ax.legend(targets)
+    ax.grid()
+
+    plt.show()
+
+
+def factor_loadings(pc, df_vars, df_pca, var1, var2):
+    '''
+    This function takes in PCA object created by a sklearn
+    decomposition, a dataframe that has all the variables,
+    a dataframe that has all the principal component vectors,
+    target variables, and returns a dataframe that explains
+    the factor loadings for each of the principal component
+    vectors created
+    '''
+
+    loadings_array = pc.components_.T * np.sqrt(pc.explained_variance_)
+
+    loadings = pd.DataFrame(data=loadings_array,
+        index= [c for c in df_vars if c != var1 and c != var2],
+        columns= [p for p in df_pca if p != var1 and p != var2])
+
+    return loadings.style.apply(highlight_vals)
+
+
+def highlight_vals(s):
+    '''
+    This is a helper function that takes in a dataframe series,
+    finds the values in that series larger than a threshold (0.5)
+    , and highlights those values.
+
+    I use it here to highlight the variables that explain most of
+    the variance within a principal component vector.
+    '''
+
+    return ['background-color: yellow' if v >= 0.5 else '' for v in s]
